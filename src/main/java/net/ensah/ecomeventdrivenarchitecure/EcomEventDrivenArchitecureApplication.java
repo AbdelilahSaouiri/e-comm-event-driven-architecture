a9861f66f9d6449c1ae8baa1fcede782364179e1
abdelilah <abdelilah.saouir@etu.uae.ac.ma>
2025-01-18 16:58:12 +0100

    first commit

---
+++ src/main/java/net/ensah/ecomeventdrivenarchitecure/EcomEventDrivenArchitecureApplication.java
@@ -0,0 +1,13 @@
+package net.ensah.ecomeventdrivenarchitecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomEventDrivenArchitecureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomEventDrivenArchitecureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ensah/ecomeventdrivenarchitecure/EcomEventDrivenArchitecureApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ensah.ecomeventdrivenarchitecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomEventDrivenArchitecureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
